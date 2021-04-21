6b691c210a77af64161bf610dbbe7435550cf065
Kartikeya Jois <kartijois@gmail.com>
2021-04-21 13:57:34 +0530

    first shell script commit

---
+++ failed_API_calls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This script will parse through given log files and store the return status of the failed API calls as well as the URLS of non successful API calls
+
+#check and see if a command line arguement has been given
+if [[ ${#} -eq 0 ]]
+then
+	echo  "Please supply location to log file as an arguement"
+	exit 1
+fi
+
+#store the file location from the command line
+
+FILE_L=${1}
+
+#obtain the error codes
+grep status_code ${FILE_L} | awk '{print $NF}' > failed_status.txt
+
+#obtain the URL 
+grep ERROR ${FILE_L} | grep http | awk -F- '{print $4}'|awk '{print $1}'|tee failed_api_calls.txt | awk -F? '{print $2}'>failed_parameters.txt
+	
+
+exit 0
